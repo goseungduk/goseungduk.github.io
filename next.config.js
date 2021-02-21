7123650085127fab6b89df3f2e8af201f1282a55
goseungduk <a42873410@gmail.com>
2021-02-21 23:27:08 +0900

    origin_file

---
+++ next.config.js
@@ -0,0 +1,5 @@
+const debug = process.env.NODE_ENV !== 'production';
+const name = 'portfolio'
+module.exports = {
+  assetPrefix: !debug ? '' : ''
+};
---
+++ pages/index.jsx
@@ -0,0 +1,35 @@
+import React, { useRef,useEffect } from "react";
+import {gsap,TweenMax} from 'gsap';
+
+const Loader = () => {
+  
+  const best=useRef(null);
+  const tl=gsap.timeline();
+
+  useEffect(()=>{
+    tl.from(best.current.children[0].children, {duration:1, y: 800,opacity:0.5}, "+=1");
+    tl.to(best.current.children[0].children,{duration:2, opacity:1,stagger:0.2});
+  }) // fromTo(target, duration, {fromVars}, {toVars})
+
+  return (
+    <div>
+      <div ref={best} id="title">
+      <h1>
+        <span>B</span>
+        <span>E</span>
+        <span>S</span>
+        <span>T</span>
+        <span>_</span>
+        <span>H</span>
+        <span>A</span>
+        <span>C</span>
+        <span>K</span>
+        <span>E</span>
+        <span>R</span>
+      </h1>
+    </div>
+  </div>
+  );
+}
+
+export default Loader;
