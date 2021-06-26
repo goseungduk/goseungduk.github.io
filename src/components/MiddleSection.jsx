a1ee71adcb6891c54909d3f72a9ba3d8adec2306
goseungduk <a457581@catholic.ac.kr>
2021-06-27 04:53:07 +0900

    210627_profile_ing

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {ReactComponent as Location} from '../statics/location.svg';
+import {ReactComponent as Mail} from '../statics/mail1.svg';
+import Name from '../statics/name.svg';
+import Group from '../statics/group.svg';
 
 const MiddleSection = () => {
   return (
@@ -9,8 +12,20 @@ const MiddleSection = () => {
         <div>
           <ul>
             <li style={{verticalAlign:"center"}}>
-              <Location style={{float:'left'}} width="30px" height="30px"></Location>
-              <span style={{fontSize:25,marginLeft:"1%"}}>인천광역시, 대한민국</span>
+              <img src={Name} style={{float:'left'}} width="33px" alt="name"></img>
+              <span style={{fontSize:25,marginLeft:"1%"}}>이승형</span>
+            </li>
+            <li style={{verticalAlign:"center"}}>
+              <Location style={{float:'left'}} width="32px" height="30px"></Location>
+              <span style={{fontSize:25,marginLeft:"1%"}}>인천광역시, 대한민국 (South Korea)</span>
+            </li>
+            <li style={{verticalAlign:"center"}}>
+              <Mail style={{float:'left'}} width="35px" height="30px"></Mail>
+              <span style={{fontSize:25,marginLeft:"1%"}}><a href="mailto:a42873410@gmail.com">a42873410@gmail.com</a></span>
+            </li>
+            <li style={{verticalAlign:"center"}}>
+              <img src={Group} style={{float:'left'}} width="36px" alt="name"></img>
+              <span style={{fontSize:25,marginLeft:"1%"}}><a href="http://catsecurity.net">CAT-Security</a>    임원</span>
             </li>
           </ul>
         </div>
