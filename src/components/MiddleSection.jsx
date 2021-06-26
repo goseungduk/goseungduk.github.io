53c393af542e627aa0dcda2ecbe6e670221b6663
goseungduk <a457581@catholic.ac.kr>
2021-06-27 02:30:04 +0900

    210627_middleSectionDesign

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {ReactComponent as Location} from '../statics/location.svg';
 
 const MiddleSection = () => {
   return (
     <div className="middleSections">
       <div id="profile" style={{height: 500}}>
         <h1>PROFILE</h1>
+        <div>
+          <ul>
+            <li style={{verticalAlign:"center"}}>
+              <Location style={{float:'left'}} width="30px" height="30px"></Location>
+              <span style={{fontSize:25,marginLeft:"1%"}}>인천광역시, 대한민국</span>
+            </li>
+          </ul>
+        </div>
       </div>
       <div id="education" style={{height: 500}}>
         <h1>Education</h1>
