a122b60cb6aeaf52a66ffa432c15942f6ab03512
goseungduk <a457581@catholic.ac.kr>
2021-06-28 20:10:28 +0900

    210628_profile_army

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as Mail} from '../statics/mail1.svg';
 import Name from '../statics/name.svg';
 import Group from '../statics/group.svg';
 import Graduation from '../statics/graduation2.svg';
+import ROKA from '../images/roka.png';
 
 const MiddleSection = () => {
   return (
@@ -25,8 +26,15 @@ const MiddleSection = () => {
               <span style={{fontSize:20,marginLeft:"1%"}}><a href="mailto:a42873410@gmail.com">a42873410@gmail.com</a></span>
             </li>
             <li style={{verticalAlign:"center"}}>
-              <img src={Group} style={{float:'left'}} width="36px" alt="group"></img>
+              <img src={Group} style={{float:'left'}} width="36px" height="33px" alt="group"></img>
               <span style={{fontSize:20,marginLeft:"1%"}}><a href="http://catsecurity.net">CAT-Security</a> 소속</span>
+            </li>
+            <li style={{verticalAlign:"center",marginBottom:"10px"}}>
+              <img src={ROKA} style={{float:'left',marginLeft:'5px'}} width="28px" height="28px" alt="group"></img>
+              <span style={{fontSize:20,marginLeft:"1%"}}>대한민국 육군(ROKA) 병장 만기전역</span>
+            </li>
+            <li>
+              <span style={{fontSize:15,marginLeft:"1%",fontWeight:'bold',fontStyle:'italic'}}>정보보호병(2020.01~2021.07)</span>
             </li>
           </ul>
         </div>
